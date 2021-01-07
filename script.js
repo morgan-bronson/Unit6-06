49c4890d70f26e056ba1b1dcb5e99806e1444f7b
morganbronson <morgan.bronson@ocsbstudent.ca>
2021-01-07 20:28:27 +0000

    comments

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ document.getElementById('mybtn').addEventListener('click', ageguess)
 
 function ageguess () {
   let age = 14
+  // assigns a variable to age
   let guess = 0
+  // assigns a variable to guess
   while (guess != age) {
     guess=parseInt(prompt("Guess my age"));
+    // opens a prompt
     if (guess == age) {
       alert('correct')
+      // opens an alert
     } else if (guess < age) {
       alert('too young, try again!')
+      // opens an alert
     } else if (guess > age) {
       alert('too old, try again!')
+      // opens an alert
     }
   }
 }
